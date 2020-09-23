332686dbd0ab462926d08fe638c8197a51a14720
Jose Bustillo <josebustillodealba@gmail.com>
2020-09-23 14:48:53 -0500

    feat: model Student

---
+++ app/src/main/java/com/jabustillo/webservice/model/Student.kt
@@ -0,0 +1,12 @@
+package com.jabustillo.webservice.model
+
+class Student (
+    var id: String,
+    var user: String,
+    var username: String,
+    var email: String,
+    var phone: String,
+    var city: String,
+    var country: String,
+    var birthday: String,
+)
