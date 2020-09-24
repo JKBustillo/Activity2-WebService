95b2e3230e287b7b5473d7e785ff2885eeff1e15
Jose Bustillo <josebustillodealba@gmail.com>
2020-09-24 06:57:34 -0500

    feat: get id

--- app/src/main/java/com/jabustillo/webservice/ui/CourseAdapter.kt
+++ app/src/main/java/com/jabustillo/webservice/ui/CourseAdapter.kt
@@ -1,15 +1,17 @@
 package com.jabustillo.webservice.ui
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.jabustillo.webservice.R
 import com.jabustillo.webservice.model.Course
 import kotlinx.android.synthetic.main.course_item.view.*
 
-class CourseAdapter(items: ArrayList<Course>): RecyclerView.Adapter<CourseAdapter.ViewHolder>() {
+class   CourseAdapter(items: ArrayList<Course>): RecyclerView.Adapter<CourseAdapter.ViewHolder>() {
     var items : ArrayList<Course>? = null
     var viewHolder: ViewHolder? = null
 
@@ -21,6 +23,10 @@ class CourseAdapter(items: ArrayList<Course>): RecyclerView.Adapter<CourseAdapte
         val view = LayoutInflater.from(parent?.context).inflate(R.layout.course_item, parent, false)
 
         viewHolder = ViewHolder(view)
+
+        view.findViewById<ConstraintLayout>(R.id.course_item).setOnClickListener{
+            Log.d("message", it.tvCourseId.text.toString())
+        }
 
         return viewHolder!!
     }
